02a989a138cf39ccdb642e2018af34acb05bc4d8
smaximov <s.b.maximov@gmail.com>
2016-07-24 17:24:32 +1000

    Rewrite command line parsing

--- org-doc-cli.el
+++ org-doc-cli.el
@@ -23,9 +23,53 @@
 
 ;;; Code:
 
+(require 'org)
 (require 'subr-x)
 
 (require 'org-doc)
+
+(defconst org-doc::cli-args-spec
+  ;; ((NAME...) DEFAULT KEY OPTION-P VALIDATE HANDLER DATUM...)
+  `((("-h" "--help") nil nil nil nil org-doc::usage)
+    (("-v" "--version") nil nil nil nil org-doc::version)
+    (("-n" "--dry-run") nil :dry-run)
+    (("-q" "--silent") nil :silent)
+    (("-s" "--section") "commentary" :section t ,org-doc::section-names))
+  "Specification of command line arguments.
+
+Each element has the form
+    ((NAME...) DEFAULT KEY OPTION-P VALIDATOR HANDLER DATUM...)
+where
+    - NAME is the argument name string (e.g., \"-h\", \"--help\");
+    - DEFAULT is the default value of the argument, if defined;
+    - KEY (optional) is a keyword which defines the key under which
+      the argument's value will be stored in the parsed arguments plist;
+    - OPTION-P is a boolean indicating whether the argument
+      is an option (takes value) or a flag;
+    - VALIDATOR (optional) is either
+          - a function to validate and transform the value;
+          - a list of valid values;
+          - an association list to map valid values to transformed values;
+    - HANDLER (optional) is either a function to invoke or a signal to fire
+      when the argument is encountered;
+    - DATUM (optional) extra arguments to pass when invoking HANDLER.")
+
+(defconst org-doc::cli-args-alist
+  (-mapcat (-lambda ((names _ . rest))
+             (-map (lambda (name)
+                     `(,name ,@rest))
+                   names))
+           org-doc::cli-args-spec))
+
+(defconst org-doc::cli-args-defaults-plist
+  (let (defaults-plist)
+    (-each org-doc::cli-args-spec
+      (-lambda ((_ default key . _))
+        (when (and default key)
+          (message "default %s key %S" default key)
+          (setf defaults-plist
+                (plist-put defaults-plist key default)))))
+    defaults-plist))
 
 (defconst org-doc::usage
   (format "org-doc %s
@@ -61,6 +105,41 @@ Options:
 (define-error 'org-doc::usage "")
 (define-error 'org-doc::version "")
 
+(defun org-doc::handle (handler value datum)
+  "Invoke HANDLER with VALUE and extra data DATUM.
+
+HANDLER is either a function or a name of a signal defined
+with `define-error'.  If HANDLER is none of the above, raise an error."
+  (cond ((get handler 'error-conditions)
+         (signal handler datum))
+        ((functionp handler)
+         (apply handler value datum))
+        (t
+         (error "Invalid handler: %S" handler))))
+
+(defun org-doc::validate (value validator)
+  "Validate (and transform) VALUE using VALIDATOR.
+
+VALIDATOR should be either
+    - a function to return a transformed value
+      or nil if the value is invalid;
+    - a list of of valid values;
+    - an association list (VALUE . TRANSFORMED-VALUE) where
+      VALUE is a valid value and TRANSFORMED-VALUE is
+      the value it should be mapped to.
+
+The function returns the transformed value or nil
+if VALUE is invalid."
+  (cond ((functionp validator)
+         (funcall validator value))
+        ((listp validator)
+         (-when-let (pair (assoc-string value validator))
+           (if (consp pair)
+               (cdr pair)
+             pair)))
+        (t
+         (error "Invalid validator: %S" validator))))
+
 (defun org-doc::parse-args (args)
   "Parse command line arguments ARGS.
 
@@ -73,53 +152,76 @@ Result is a property list
   (setf args (-remove-item "--" args))
   (when (null args)
     (signal 'org-doc::usage nil))
-  (let ((positional-args-left 2)
-        org elisp section silent dry-run)
+  (let (positional args-plist)
     (while args
       (let ((argi (pop args))
             value)
+        ;; normalize long options
         (when (string-match "^\\(--[^=]+\\)=" argi)
           (setf value (substring argi (match-end 0))
                 argi (match-string 1 argi))
-          (when (string-blank-p value)
-            (signal 'org-doc::missing-value
-                    (format "options `%s' requires a value" argi))))
-        (cond ((member argi '("-h" "--help"))
-               (signal 'org-doc::usage nil))
-              ((member argi '("-v" "--version"))
-               (signal 'org-doc::version nil))
-              ((member argi '("-n" "--dry-run"))
-               (setf dry-run t))
-              ((member argi '("-q" "--silent"))
-               (setf silent t))
-              ((member argi '("-s" "--section"))
-               (when section
-                 (signal 'org-doc::duplicate-argument
-                         "option `--section' defined multiple times"))
-               (unless (setf value (or value (pop args)))
-                 (signal 'org-doc::missing-value
-                         "option `--section' requires a value"))
-               (unless (org-doc::valid-section-name? value)
-                 (signal 'org-doc::invalid-option-value
-                         (format "invalid value for option `--section': expected one of %s; `%s' given"
-                                 org-doc::section-names value)))
-               (setf section value))
-              ((string-prefix-p "-" argi)
-               (signal 'org-doc::unknown-argument
-                       (format "unknown option or flag: `%s'" argi)))
 
-              ;; treat rest values as positional arguments
-              (t (push argi positional)))))
+          (unless (or (null value)
+                      (string-empty-p value))
+            (push value args)))
+
+        (-if-let (arg-spec (assoc argi org-doc::cli-args-alist))
+            ;; handle known arguments
+            (let ((key (nth 1 arg-spec))
+                  (option? (nth 2 arg-spec))
+                  (validator (nth 3 arg-spec))
+                  (handler (nth 4 arg-spec))
+                  (datum (nthcdr 5 arg-spec)))
+
+              ;; handle duplicate arguments
+              (when (plist-get args-plist key)
+                (signal 'org-doc::duplicate-argument
+                        (format "option `%s' defined multiple times"
+                                argi)))
+
+              ;; handle options
+              (when option?
+                (setf value (pop args))
+
+                ;; missing values
+                (unless value
+                  (signal 'org-doc::missing-value
+                          (format "option `%s' requires a value"
+                                  argi)))
+
+                ;; validation
+                (when validator
+                  (setf value
+                        (or (org-doc::validate value validator)
+                            (signal 'org-doc::invalid-option-value
+                                    (format "invalid value for option `%s': %s"
+                                            argi value))))))
+
+              (when key
+                (setf args-plist
+                      (plist-put args-plist key (if option? value t))))
+
+              (when handler
+                (org-doc::handle handler (if option? value t) datum)))
+          ;; handle unknown arguments
+          (if (string-prefix-p "-" argi)
+              ;; unknown options and flags
+              (signal 'org-doc::unknown-argument
+                      (format "unknown option or flag: `%s'" argi))
+
+            ;; treat rest values as positional arguments
+            (push argi positional)))))
+
     (when (/= 2 (length positional))
       (signal 'org-doc::positional-arg-count-mismatch
               (format "expected exactly 2 positional arguments; %s given"
                       (length positional))))
     (setf positional (reverse positional))
-    (list :org (nth 0 positional)
-          :elisp (nth 1 positional)
-          :section (or section "commentary")
-          :dry-run dry-run
-          :silent silent)))
+
+    (setf args-plist (plist-put args-plist :org (nth 0 positional)))
+    (setf args-plist (plist-put args-plist :elisp (nth 1 positional)))
+
+    (org-combine-plists org-doc::cli-args-defaults-plist args-plist)))
 
 (defun org-doc::usage (&optional exit-code)
   "Display `org-doc' usage information and exit.
